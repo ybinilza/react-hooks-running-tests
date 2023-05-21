1e7001c7cac833de8e56870549615809c72721a6
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-05-20 20:32:50 -0400

    lab Completed

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,6 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
